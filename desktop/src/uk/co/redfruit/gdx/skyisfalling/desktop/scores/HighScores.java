28cdb143544b4ff48ccf422a113821d03a7208c8
Paul Hunnisett <paul@redfruit.co.uk>
2016-11-29 15:00:55 +0000

    Began implementing high scores for Desktop.  Implementation is backed by a small json file that is ordered and limited to 5 values.

---
+++ desktop/src/uk/co/redfruit/gdx/skyisfalling/desktop/scores/HighScores.java
@@ -0,0 +1,13 @@
+package uk.co.redfruit.gdx.skyisfalling.desktop.scores;
+
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Created by paul on 29/11/16.
+ */
+
+public class HighScores {
+
+    public Array<Integer> scores = new Array<>(true, 5);
+
+}
