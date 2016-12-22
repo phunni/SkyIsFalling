6e2f026f9c6dda8809a4dc0cae9e0e2091ed6e70
Paul Hunnisett <paul@redfruit.co.uk>
2016-12-22 16:29:37 +0000

    removed redundant line

--- core/src/uk/co/redfruit/gdx/skyisfalling/game/objects/PlayerShip.java
+++ core/src/uk/co/redfruit/gdx/skyisfalling/game/objects/PlayerShip.java
@@ -33,7 +33,6 @@ public class PlayerShip extends GameObject {
         this.level = level;
         lives = 3;
         playerShipRegion = Assets.getInstance().getPlayer();
-        position.set(Constants.WORLD_WIDTH / 2, 0.1f);
         defaultDynamicBodyDef.position.set(position.x, position.y);
         defaultDynamicBodyDef.fixedRotation = true;
         FixtureDef playerShipFixtureDef = new FixtureDef();
